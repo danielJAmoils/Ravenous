8d644bf435dc80b1cbc131b72e09db37a72eef7a
Daniel Amoils <danieljamoils@gmail.com>
2020-07-01 21:46:52 -0400

    added state and props to buisness and business list which are passed by there parents

--- src/components/BusinessList/BusinessList.tsx
+++ src/components/BusinessList/BusinessList.tsx
@@ -2,16 +2,29 @@ import React from 'react'
 import './BusinessList.css'
 import Business from '../Business/Business'
 
-class BusinessList extends React.Component{
+type BusinessListProps = {
+    businesses: {
+        imageSrc: string;
+        name: string;
+        address: string;
+        city: string;
+        state: string;
+        zipCode: string;
+        category: string;
+        rating: number;
+        reviewCount: number;
+    }[]
+}
+
+class BusinessList extends React.Component<BusinessListProps, {}>{
     render(){
         return (
             <div className="BusinessList">
-                <Business /> 
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                this.props.businesses.map(function(business){
+                    return <Business business={business}/>
+                })
+                }
             </div>
         )
     }
