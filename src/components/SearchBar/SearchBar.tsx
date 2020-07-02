5020d58dd9b2e3200783fbc1a45107b31dbc20ee
Daniel Amoils <danieljamoils@gmail.com>
2020-07-01 23:04:28 -0400

    added handler event to search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,65 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import './SearchBar.css'
 
-const sortByOptions:{ [index: string]: string } = {//alows object to be indexed with strings
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
+type SearchBarState = {
+    term: string
+    location: string
+    sortBy: string
 }
 
-class SearchBar extends React.Component {
+class SearchBar extends React.Component<{}, SearchBarState> {
+    sortByOptions: {  [index: string]: string //allows object to be indexed with strings
+        'Best Match': string; 'Highest Rated': string; 'Most Reviewed': string;
+    }
+    constructor(props: Readonly<{}>){
+        super(props)
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        }
+
+        this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
+
+        this.sortByOptions =  {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        }
+    }
+
+    getSortByClass(sortByOption: string){
+        if(sortByOption === this.state.sortBy){
+            return 'active'
+        }else{
+            return ''
+        }
+    }
+
+    handleSortByChange(sortByOption: string){
+        this.setState({sortBy: sortByOption})
+    }
+
+    handleTermChange(event:any){//try to find better type look below it is on change
+        this.setState({
+            term:event.target.value
+        })
+    }
+
+    handleLocationChange(event:any){//is a change event i don't know why not working
+        this.setState({
+            location:event.target.value
+        })
+    }
+
     renderSortByOptions(){
-        return Object.keys(sortByOptions).map((sortByOption) => {
-            const sortByOptionValue = sortByOptions[sortByOption]
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map((sortByOption) => {
+            const sortByOptionValue = this.sortByOptions[sortByOption]
+            return <li 
+            key={sortByOptionValue} 
+            className={this.getSortByClass(sortByOptionValue)}
+            onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>{sortByOption}</li>
         })
     }
 
@@ -20,12 +68,12 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+                    <input placeholder="Where?" onChange={this.handleLocationChange}/>
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
